5f98da12df0e495dcbf59d0fb20ac1f138f4a03c
DESKTOP-7FC6AMI\intern4-cosmin <duna.marius.cosmin@gmail.com>
2017-08-24 13:41:46 +0300

    added build folder

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,7 +1,7 @@
 git config user.name "DunaMariusCosmin";
 git config user.email "duna.marius.cosmin@gmail.com";
 git fetch;
-git checkout master;
+git -f checkout master;
 git reset;
 cp -f target/addon.xml build;
 git add build/addon.xml;
